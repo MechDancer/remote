cf433db52f3a546bac52d6bad046894d43774fd0
YangDR <ydrml@hotmail.com>
2018-11-14 14:11:37 +0800

    添加文档
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- src/main/kotlin/org/mechdancer/remote/core/GroupManager.kt
+++ src/main/kotlin/org/mechdancer/remote/core/GroupManager.kt
@@ -17,16 +17,7 @@ class GroupManager {
 
 	/**
 	 * 获取成员的沉默时间
-	 * @param name 要获取的成员名字
-	 * @return 若成员存在，最后一次出现时间到当前的毫秒数
-	 *         若成员不存在，返回当前时间的总毫秒数
-	 */
-	fun silenceTime(name: String) =
-		System.currentTimeMillis() - (group[name] ?: 0)
-
-	/**
-	 * 获取成员的沉默时间
-	 * @param names 感兴趣的成员名录，不写表示获取所有成员
+	 * @param names 感兴趣的成员名单，不写表示获取所有成员
 	 * @return 所有感兴趣成员的沉默时间
 	 */
 	fun silenceTime(vararg names: String): Map<String, Long> {
--- src/main/kotlin/org/mechdancer/remote/core/SignalBlocker.kt
+++ src/main/kotlin/org/mechdancer/remote/core/SignalBlocker.kt
@@ -6,9 +6,16 @@ package org.mechdancer.remote.core
 internal class SignalBlocker {
 	private val core = Object()
 
+	/**
+	 * 阻塞等待信号
+	 * @param timeout 用毫秒表示的阻塞时间
+	 */
 	fun block(timeout: Long = 0) =
 		synchronized(core) { core.wait(timeout) }
 
+	/**
+	 * 唤醒所有阻塞的线程
+	 */
 	fun awake() =
 		synchronized(core) { core.notifyAll() }
 }
