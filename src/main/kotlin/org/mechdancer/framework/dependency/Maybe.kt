c275dfb05a629c0c45ee74cf28d563c99c309c36
YangDR <ydrml@hotmail.com>
2018-11-23 17:51:24 +0800

    为代理属性添加线程安全性
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- src/main/kotlin/org/mechdancer/framework/dependency/Maybe.kt
+++ src/main/kotlin/org/mechdancer/framework/dependency/Maybe.kt
@@ -9,7 +9,13 @@ import kotlin.reflect.KProperty
 class Maybe<out T : Dependency>(
     private val block: () -> T?
 ) : ReadOnlyProperty<Dependency, T?> {
+
     private var field: T? = null
-    override fun getValue(thisRef: Dependency, property: KProperty<*>) =
-        field ?: block()?.also { field = it }
+
+    override fun getValue(
+        thisRef: Dependency,
+        property: KProperty<*>
+    ) = field ?: synchronized(block) {
+        block()?.also { field = it }
+    }
 }
