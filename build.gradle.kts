d8bc876e08086b79561ff27b8b0ae04c411136fb
YangDR <ydrml@hotmail.com>
2018-12-11 18:31:14 +0800

    修改kotlin版本
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,69 +3,69 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
-	repositories {
-		mavenCentral()
-		jcenter()
-	}
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
 
-	dependencies {
-		classpath("org.jetbrains.dokka:dokka-gradle-plugin")
-		classpath("com.novoda:bintray-release:+")
-	}
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin")
+        classpath("com.novoda:bintray-release:+")
+    }
 }
 
 plugins {
-    kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.11"
     id("org.jetbrains.dokka") version "0.9.16"
 }
 
 apply {
-	plugin("com.novoda.bintray-release")
+    plugin("com.novoda.bintray-release")
 }
 
 group = "org.mechdancer"
 version = "0.2.1-dev-4"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	compile(kotlin("stdlib-jdk8"))
-	testCompile("junit", "junit", "+")
+    compile(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "+")
     compile(kotlin("reflect"))
 }
 
 configure<JavaPluginConvention> {
-	sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
 }
 tasks.withType<KotlinCompile> {
-	kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 configure<PublishExtension> {
-	userOrg = "mechdancer"
-	groupId = "org.mechdancer"
-	artifactId = "remote"
-	publishVersion = version.toString()
-	desc = "communication lib"
-	website = "https://github.com/MechDancer/remote"
-	setLicences("WTFPL")
+    userOrg = "mechdancer"
+    groupId = "org.mechdancer"
+    artifactId = "remote"
+    publishVersion = version.toString()
+    desc = "communication lib"
+    website = "https://github.com/MechDancer/remote"
+    setLicences("WTFPL")
 }
 
 task<Jar>("sourceJar") {
-	classifier = "sources"
-	from(sourceSets["main"].allSource)
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
 }
 
 task<Jar>("javadocJar") {
-	classifier = "javadoc"
-	from("$buildDir/javadoc")
+    classifier = "javadoc"
+    from("$buildDir/javadoc")
 }
 
 tasks.withType<DokkaTask> {
-	outputFormat = "javadoc"
-	outputDirectory = "$buildDir/javadoc"
+    outputFormat = "javadoc"
+    outputDirectory = "$buildDir/javadoc"
 }
 
 tasks["javadoc"].dependsOn("dokka")
