65da07bf3769decbad58912bcf24cc70bee8cbda
berberman <1793913507@qq.com>
2018-12-30 09:57:29 +0800

    清空仓库

--- src/main/kotlin/org/mechdancer/framework/dependency/TagComponent.kt
+++
@@ -1,9 +0,0 @@
-package org.mechdancer.framework.dependency
-
-/**
- * 标记的组件
- * 可通过字符串查找
- */
-interface TagComponent {
-    val tag: String
-}
--- src/main/kotlin/org/mechdancer/framework/remote/modules/tcpconnection/MailListener.kt
+++
@@ -1,7 +0,0 @@
-package org.mechdancer.framework.remote.modules.tcpconnection
-
-import org.mechdancer.framework.dependency.Component
-
-interface MailListener : Component {
-    fun process(sender: String, payload: ByteArray)
-}
